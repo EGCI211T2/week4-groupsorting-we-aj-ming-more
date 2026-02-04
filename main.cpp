ed458120f36009cb19a7af2ef94c0513f300ae16
constantinos030 <constantinos.joh@student.mahidol.edu>
2026-02-04 11:35:58 +0000

    Only quicksort works

--- main.cpp
+++ main.cpp
@@ -6,25 +6,27 @@ using namespace std;
 
 int main(int argc, char**argv) {
   int N = argc-2; // N = the array length. Do N - 1 when using it as an index.
+  string type = argv[1]; // type = sorting type
   int *a = new int[N];
-  /* Make sure you convert 'a' properly*/ 
+  /* Make sure you convert 'a' properly*/ // i conv
   for (int i = 2; i<argc; i++)
     {
       a[i-2] = atoi(argv[i]);
     }
 
    display(a,N);
- cout << argv[1] <<endl;
+ cout << type <<endl;
   if(argc>1){
-       if      (strcmp(argv[1],"bubble")==0) bubbleSort(a,N); 
-       else if (strcmp(argv[1],"selection")==0) selectionSort(a,N);
-       else if (strcmp(argv[1],"insert")==0) insertion(a,N);
+       if      (type=="quick")     quicksort(a, 0, N-1); //check sorting.h for explanation! - Cj. I used the GeeksforGeeks quicksort algorithm.
+       else if (type=="selection") selectionSort(a,N);
+       else if (type=="insert")    insertion(a,N);
+       else if (type=="bubble")    bubbleSort(a,N);
        else    {cout<<"Invalid Sorting Type."; return -1;}
       // selectionSort(a,N);
        display(a,N);
       }
 
- delete a;
+ delete[] a;
  return 0;
 }
 
