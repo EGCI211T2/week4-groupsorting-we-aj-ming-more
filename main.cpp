05acbc80c99b5cbbf16c76db03c16794f2535f58
minnggii <54227292+minnggii@users.noreply.github.com>
2026-02-05 16:52:00 +0000

    got selectionsort to work :D

--- main.cpp
+++ main.cpp
@@ -1,24 +1,31 @@
 #include <iostream>
 #include <cstring>
+
 using namespace std;
 #include "sorting.h"
 
 int main(int argc, char**argv) {
-  int *a,N;
-  /* Make sure you convert 'a' properly*/ 
-  
+  int N = argc-2; // N = the array length. Do N - 1 when using it as an index.
+  string type = argv[1]; // type = sorting type
+  int *a = new int[N];
+  /* Make sure you convert 'a' properly*/ // i conv
+  for (int i = 2; i<argc; i++)
+    {
+      a[i-2] = atoi(argv[i]);
+    }
 
    display(a,N);
-
+ cout << type <<endl;
   if(argc>1){
-       if(argv[1] =="bubble") bubbleSort(a,N); 
-       else /* Complete the rest*/
-         //insertion(a,N);
-    
-      // selectionSort(a,N);
+       if      (type=="quick")     quicksort(a, 0, N-1); //check sorting.h for explanation! - Cj. I used the GeeksforGeeks quicksort algorithm.
+       else if (type=="selection") selectionSort(a,N);
+       else if (type=="insert")    insertion(a,N);
+       else if (type=="bubble")    bubbleSort(a,N);
+       else    {cout<<"Invalid Sorting Type."; return -1;}
+       //selectionSort(a,N);
        display(a,N);
       }
+
+ delete[] a;
  return 0;
 }
-
-
--- sorting.h
+++ sorting.h
@@ -1,9 +1,16 @@
-#ifndefine SORTING_H
+#ifndef SORTING_H
 #define SORTING_H
 
 #include <iomanip>
 /* function prototype*/
 
+void swap(int &a,int &b){
+ int temp;
+  temp=a;
+  a=b;
+  b=temp;
+  
+}
 
 void display(int a[],int n){
 
@@ -16,28 +23,30 @@ void display(int a[],int n){
   
 }
 
-void selectionSort(int data[], int length) 
+void selectionSort(int *a, int length) 
 { 
-	int i, j, m, mi; 
-	
-     display(data,length);
+  int i, j;
+  int *p;
+  for(j = 0; j < length-1; j++)
+  {
+      p = &a[j];
+      for(i = j+1; i < length; i++)
+      {
+          if(*p > a[i])
+          {
+              p = &a[i];
+          }
+      }
+  swap(a[j], *p);
+  }
      
-	
 } 
-
 
 
 void insertion(int a[],int n){
 
-
-}
-
-void swap(int &a,int &b){
- int temp;
-  temp=a;
-  a=b;
-  b=temp;
-  
+cout << "insert" << std::endl;
+display(a, n);
 }
 
 void bubbleSort(int a[],int n){
@@ -45,16 +54,46 @@ void bubbleSort(int a[],int n){
 int i,j;
 int sorted;
 // how may pair to compare?
+cout << "bubble" << std::endl;
 for(j=1;j<n;j++){
 
-
-	
-  display(a,n);
- }
-  
+display(a,n);
+ }  
 }
+
+
+// Quicksort Algorithm, 6880815 CJ. 
+int partition(int a[],int first,int last) // first can be omitted and 0 used in its place,
+{                                   
+// Chooses the last element as the pivot value for the array
+int pivot=a[last]; 
+ //chooses the element prior to the first element in the array
+ int i = first-1; 
+
+
+ for (int j = first; j <= last - 1; j++)  //This loop checks if a[j] is less than the pivot value,
+ {                                        // incrementing i, then swapping the value. If greater, j increments, no swap happens.
+  if (a[j]<pivot){i++; swap(a[i], a[j]);}
+ }
+
+ swap(a[i+1], a[last]); //swapping the final element with the element after i.
+ return i + 1;
+}
+
+// the quicksort function calls partition() recursively, splitting the elements partitioned until it is sorted.
+void quicksort(int a[],int first,int last)
+{
+  if (first < last)
+  {
+    int pi = partition(a, first, last);
+
+    quicksort(a, first, pi-1); // These split the previously partitioned array into two halves, sorted, then split again, recursively.
+    quicksort(a, pi+1, last);
+  }
+
+}
+
+
+
+
 #endif
-
-
-
-
